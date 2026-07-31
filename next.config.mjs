632793210d3b067a019e24f33d989f3aba5446a4
asiaroly <a.k.rolybd@gmail.com>
2026-07-31 21:36:03 +0600

    Fix next.config and environment setup

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export', // <--- Add this line
-  images: {
-    unoptimized: true, // Required for static export if using Next <Image />
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
+  
 };
 
-module.exports = nextConfig;
+export default nextConfig;
